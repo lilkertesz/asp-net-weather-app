61a7967693023bc6a1069ea233a9bdcf8c256a95
Lilla Kertesz <lil.kertesz@gmail.com>
2020-12-03 13:41:51 +0100

    Create InMemoryObservationRepo and fill it with dummy data

---
+++ WeatherApp/WeatherApp.Webpage/Models/InMemoryObservationsRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeatherApp.WebSite.Models
+{
+    public class InMemoryObservationsRepository : IObservationRepository
+    {
+        IList<Observation> _observations = new List<Observation>() {
+            new Observation{
+                ID = 1,
+                City = "Budapest",
+                TimeStamp = new DateTime(2020, 11, 14, 9, 28, 0),
+                UserName = "User",
+                Description = "It is very hot and sunny here"
+            },
+            new Observation{
+                ID = 2,
+                City = "Budapest",
+                TimeStamp = new DateTime(2020, 10, 14, 9, 28, 0),
+                UserName = "Jane",
+                Description = "Freezing cold"
+            },
+            new Observation{
+                ID = 3,
+                City = "Madrid",
+                TimeStamp = new DateTime(2020, 6, 14, 9, 28, 0),
+                UserName = "Pablo",
+                Description = "Beach time!"
+            }
+        };
+
+        public void Create(Observation observation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long observationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Observation> Read()
+        {
+            return _observations;
+        }
+
+        public void Update(long observationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WeatherApp/WeatherApp.Webpage/Models/Observation.cs
+++ WeatherApp/WeatherApp.Webpage/Models/Observation.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WeatherApp.WebSite.Models
 {
     public class Observation
     {
+        public long ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string City { get; set; }
+        public string UserName { get; set; }
+        public string Description { get; set; }
     }
 }
