9d35bb97f61bec70be0b49d3f7458599a0247042
Lilla Kertesz <lil.kertesz@gmail.com>
2020-12-03 15:24:50 +0100

    Reorganize data related files into a separate folder

---
+++ WeatherApp/WeatherApp.Webpage/Data/Interfaces/IFavoritesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApp.WebSite.Models
+{
+    public interface IFavoritesRepository
+    {
+        void Create(string city);
+        ICollection<string> Read();
+        void Delete(string city);
+    }
+}
