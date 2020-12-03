274e9c3dc2bd809d4e0e4212f48550649bc133cd
Lilla Kertesz <lil.kertesz@gmail.com>
2020-12-03 13:28:12 +0100

    Add ObservationController and IObservationRepository

---
+++ WeatherApp/WeatherApp.Webpage/Controllers/ObservationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WeatherApp.WebSite.Models;
+
+namespace WeatherApp.WebSite.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ObservationController : ControllerBase
+    {
+        IObservationRepository _observationRepository;
+        public ObservationController(IObservationRepository observationRepository)
+        {
+            _observationRepository = observationRepository;
+        }
+
+        [HttpGet("{city}")]
+        public Observation[] GetObservationsByCity()
+        {
+            return _observationRepository.Read().ToArray();
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp.Webpage/Models/IObservationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherApp.WebSite.Models
+{
+    public interface IObservationRepository
+    {
+        void Create(Observation observation);
+        ICollection<Observation> Read();
+        void Update(long observationId);
+        void Delete(long observationId);
+    }
+}
