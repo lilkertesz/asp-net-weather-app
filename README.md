1c4a58e3f8c6b00dbcf79ee10830ea2a53297a7c
Lilla Kertesz <lil.kertesz@gmail.com>
2021-01-07 10:37:34 +0100

    Add unit tests and rewrite readme

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-## Weather App
-ASP.Net back end of a weather forecast application (front end: https://github.com/lilkertesz/react-weather-forecast).
+# Weather Forecast Application
 
-The application fetches weather data from: https://api.openweathermap.org and city autocomplete suggestions from: https://autocomplete.geocoder.ls.hereapi.com
+Look up weather forecasts or the current weather for any location, and save favorites to track the current weather of multiple locations.
 
+front end: https://github.com/lilkertesz/react-weather-app
+
+## About
+
+![mvc](https://img.shields.io/badge/MVC_design_pattern-yellow)
+![asp.net](https://img.shields.io/badge/3.1-ASP.NET_Core-blue)
+![ef](https://img.shields.io/badge/5.0-EF_Core-purple)
+![nunit](https://img.shields.io/badge/3.11-NUnit-green)
--- WeatherApp.Tests/Controllers/WeatherControllerTest.cs
+++ WeatherApp.Tests/Controllers/WeatherControllerTest.cs
@@ -1,5 +1,6 @@
 ﻿using NSubstitute;
 using NUnit.Framework;
+using System.Collections.Generic;
 using WeatherApp.Controllers;
 using WeatherApp.Models;
 using WeatherApp.Services.Interfaces;
@@ -21,13 +22,37 @@ namespace WeatherApp.Tests.Controllers
         }
 
         [Test]
-        public void GetWeatherForecastForCity()
+        public void GetCurrentWeatherForCoordinates()
         {
-            var forecast = new Weather();
+            var weather = new Weather();
 
-            _weatherService.GetCurrentWeather(20, 10).Returns(forecast);
+            _weatherService.GetCurrentWeather(20, 10).Returns(weather);
 
             var result = _weatherController.GetCurrentWeather(20, 10);
+
+            Assert.AreEqual(result, weather);
+        }
+
+        [Test]
+        public void GetDailyForecastForCoordinates()
+        {
+            IList<Weather> forecast = new List<Weather>();
+
+            _weatherService.GetDailyForecast(20, 10).Returns(forecast);
+
+            var result = _weatherController.GetDailyForecast(20, 10);
+
+            Assert.AreEqual(result, forecast);
+        }
+
+        [Test]
+        public void GetHourlyForecastForCoordinates()
+        {
+            IList<Weather> forecast = new List<Weather>();
+
+            _weatherService.GetHourlyForecast(20, 10).Returns(forecast);
+
+            var result = _weatherController.GetHourlyForecast(20, 10);
 
             Assert.AreEqual(result, forecast);
         }
