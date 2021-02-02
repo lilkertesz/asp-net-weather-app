d4f17847306574c58962dfab86baee809af559a4
Lilla Kertesz <lil.kertesz@gmail.com>
2021-02-02 11:03:58 +0100

    Delete comments

--- WeatherApp/Models/User.cs
+++ WeatherApp/Models/User.cs
@@ -10,17 +10,11 @@ namespace WeatherApp.Models
     {
         [Required]
         public string Username { get; set; }
-
         [Required]
         [EmailAddress]
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
-        //[DataType(DataType.Password)]
-        //[Display(Name = "Confirm Password")]
-        //[Compare("Password", ErrorMessage = "Password and confirmation password not match.")]
-        //public string ConfirmPassword { get; set; }
     }
 }
